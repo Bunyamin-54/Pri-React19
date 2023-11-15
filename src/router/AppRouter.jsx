f9896ed8c5194139a10d1e6ce3a9787d753b212a
Ali <alidrl26@gmail.com>
2023-11-15 09:09:10 +0200

    up

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,31 @@
+import { Routes, Route } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Dashboard from "../pages/Dashboard"
+import About from "../pages/About"
+
+
+
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <div>
+
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path='/' element={<Dashboard />} />
+        <Route path='about' element={<About />} />
+
+
+
+      </Routes>
+
+      <Footer />
+
+    </div>
   )
 }
 

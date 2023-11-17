af85e4eed843173d8718ee65f542bf450e477a38
Ali <alidrl26@gmail.com>
2023-11-17 23:11:47 +0200

    updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, CardMedia, IconButton, Typography } from "@mui/material"
+import { Box, CardContent,Typography } from "@mui/material"
 import axios from "axios"
 import { useState } from "react"
 import { useEffect } from "react"

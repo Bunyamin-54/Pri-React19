3c749afc002c2a3e3db600e96cf5c2c585ff4150
AliDurul <alidrl26@gmail.com>
2025-10-18 22:10:32 +0200

    up

--- react-router-v7/2-route-module/app/routes.ts
+++ react-router-v7/2-route-module/app/routes.ts
@@ -47,6 +47,4 @@ export default [
     ])
 
 
-
-
 ] satisfies RouteConfig;
--- react-router-v7/2-route-module/app/routes/detail-post.tsx
+++ react-router-v7/2-route-module/app/routes/detail-post.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import type { Route } from "./+types/detail-post";
 
-export default function DetailPost() {
+export default function DetailPost({
+  loaderData,
+  actionData,
+  params,
+  matches,
+}: Route.ComponentProps) {
   return (
-    <div>DetailPost</div>
-  )
+    <div>
+      <p>Loader Data: {JSON.stringify(loaderData)}</p>
+      <p>action Data: {JSON.stringify(actionData)}</p>
+      <p>params Data: {JSON.stringify(params)}</p>
+      <p>mathes Data: {JSON.stringify(matches)}</p>
+    </div>
+  );
 }
